7420eed4c7d49c812fbd3ad6408ea1310c8db13b
narul <naruln94@mail.ru>
2020-02-04 19:48:37 +0600

    Returns items page and index page of the app, Loads data from db.txt as json

---
+++ app.py
@@ -0,0 +1,32 @@
+import json
+from flask import render_template
+from flask import request
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    '''
+    Return index page of the app
+    '''
+    return render_template('index.html')
+
+
+@app.route('/items', methods=['GET', 'POST'])
+def items():
+    '''
+    Returns items page
+    Loads data from db.txt as json
+    '''
+    with open('db.txt', 'r') as f:
+        items = json.load(f)
+        if request.method == 'POST':
+            item = request.form['item']
+            quantity = request.form['quantity']
+            items.update({item: quantity})
+            with open('db.txt', 'w') as f2:
+                json.dump(items, f2)
+        return render_template('items.html', items=items)
