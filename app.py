4f5b820adb18a6a2cf6cd7c5236970d7a6c04333
narul <naruln94@mail.ru>
2020-02-06 14:18:24 +0600

    Add .gitignore, Delete items from db.txt

--- app.py
+++ app.py
@@ -30,3 +30,19 @@ def items():
             with open('db.txt', 'w') as f2:
                 json.dump(items, f2)
         return render_template('items.html', items=items)
+
+
+@app.route('/remove_items', methods=['GET', 'POST'])
+def remove_items():
+    '''
+    Delete items and Quantity from db.txt
+    '''
+    with open('db.txt', 'r') as f:
+        items = json.load(f)
+        if request.method == 'POST':
+            item = request.form['item']
+            quantity = request.form['quantity']
+            del items[item]
+            with open('db.txt', 'w') as f2:
+                json.dump(items, f2)
+        return render_template('remove_items.html', items=items)
