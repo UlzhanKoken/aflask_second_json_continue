75c9d6d933f7355cbd79d685a0d7c26199ee49b7
narul <naruln94@mail.ru>
2020-02-10 16:18:09 +0600

    Add comments

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def get():
+    '''
+    Returns items
+    '''
     with open('db.txt') as f:
         items = json.load(f)
     return render_template(
@@ -20,6 +23,9 @@ def get():
 
 @app.route('/post', methods=['POST'])
 def post():
+    '''
+    Updates items
+    '''
     item = request.form['item']
     quantity = request.form['quantity']
     with open('db.txt') as f:
@@ -60,118 +66,3 @@ def remove_items():
             with open('db.txt', 'w') as f2:
                 json.dump(items, f2)
         return render_template('remove_items.html', items=items)
-
-
-
-
-
-
-
-
-
-
-
-
-# app = Flask(__name__)
-
-
-# @app.route('/')
-# def get():
-#     with open('db.txt') as f:
-#         tasks = json.load(f)
-#     return render_template(
-#         'index.html',
-#         tasks=tasks
-#     )
-
-
-# @app.route('/post', methods=['POST'])
-# def post():
-#     task = request.form['task']
-#     with open('db.txt') as f:
-#         tasks = json.load(f)
-#     tasks.append(task)
-#     with open('db.txt', 'w') as f:
-#         json.dump(tasks, f)
-#     return 'Success'
-
-
-
-# app = Flask(__name__)
-
-
-# @app.route('/')
-# def index():
-#     '''
-#     Return index page of the app
-#     '''
-#     return render_template('index.html')
-
-
-# @app.route('/items', methods=['GET', 'POST'])
-# def items():
-#     '''
-#     Returns items page
-#     Loads data from db.txt as json
-#     '''
-#     with open('db.txt', 'r') as f:
-#         items = json.load(f)
-#         if request.method == 'POST':
-#             item = request.form['item']
-#             quantity = request.form['quantity']
-#             items.update({item: quantity})
-#             with open('db.txt', 'w') as f2:
-#                 json.dump(items, f2)
-#         return render_template('post.html', items=items)
-
-
-# @app.route('/remove_items', methods=['GET', 'POST'])
-# def remove_items():
-#     '''
-#     Delete items and Quantity from db.txt
-#     '''
-#     with open('db.txt', 'r') as f:
-#         items = json.load(f)
-#         if request.method == 'POST':
-#             item = request.form['item']
-#             del items[item]
-#             with open('db.txt', 'w') as f2:
-#                 json.dump(items, f2)
-#         return render_template('remove_items.html', items=items)
-
-#-----------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-# @app.route('/post', methods=['GET', 'POST'])
-# def post():
-#     task = ''
-#     with open('db.txt', 'r') as f:
-#         tasks = json.load(f)
-#     if request.method == 'POST':
-#         task = request.form['data']
-#         with open('db.txt', 'w') as f2:
-#             json.dump(tasks, f2)
-#         tasks.append(task)
-#     return render_template('post.html', tasks=tasks)
-
-# @app.route('/post', methods=['POST'])
-# def post():
-#     with open('db.txt', 'r') as f:
-#         tasks = json.load(f)
-#         if request.method == 'POST':
-#             task = request.form['task']
-#             with open('db.txt', 'w') as f2:
-#                 json.dump(tasks, f2)
-#             tasks.append(task)
-#         return render_template('post.html', tasks=tasks)
-
-
