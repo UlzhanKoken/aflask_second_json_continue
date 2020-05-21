969787b482ff97f7ec2244cab0591e2b6fed139b
Ulzhan <kokenova.ulzhan@gmail.com>
2020-05-21 17:34:02 +0600

    last

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import random
 from flask import render_template
 from flask import request
 from flask import Flask
@@ -66,3 +67,39 @@ def remove_items():
             with open('db.txt', 'w') as f2:
                 json.dump(items, f2)
         return render_template('remove_items.html', items=items)
+
+
+@app.route('/surprise')
+def surprise():
+    with open('db.txt') as f:
+        items = json.load(f)
+        rand = random.choice(list(items))
+    return render_template('surprise.html', rand=rand)
+
+
+# @app.route('/lottery', methods=["POST"])
+# def lottery():
+#     item = request.form['item']
+#     quantity = request.form['quantity']
+#     with open('db2.txt', 'r') as f:
+#         items = json.load(f)
+#         rand = random.choice(list(items))
+#     items.update({item: quantity})
+#     with open('db2.txt', 'w') as f:
+#         json.dump(items, f)
+#     return render_template('lottery.html', items=items, rand=rand)
+
+@app.route('/lottery', methods=['GET', 'POST'])
+def lottery():
+    with open('db2.txt') as f:
+        items = json.load(f)
+        if request.method == 'POST':
+            item = request.form['item']
+            quantity = request.form['quantity']
+            items.update({item: quantity})
+        with open('db2.txt', 'w') as f:
+            json.dump(items, f)
+        winner = None
+        if len(items) > 5:
+            winner = random.choice(list(items))
+        return render_template('lottery.html', items=items, winner=winner)
